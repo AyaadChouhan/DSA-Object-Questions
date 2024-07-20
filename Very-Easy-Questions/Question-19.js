73ca7c1d50d26f1f115b326f427d0955a5fea999
Ayaad Chouhan <136963590+AyaadChouhan@users.noreply.github.com>
2024-07-20 19:35:00 +0530

    Add files via upload

---
+++ Very-Easy-Questions/Question-19.js
@@ -0,0 +1,24 @@
+// Find the Bug: Returning Valid Units of Measure
+// There has been a masterdata issue which affected the unit of measure of the products. All values need to be checked if they are valid. The unit of measure is valid when it is either "L" (liters), "PCE" (pieces) OR when the product has a comment.
+
+// The return value should be a Boolean.
+
+// Expected results
+// hasValidUnitOfMeasure({ "product": "Milk", unitOfMeasure: "L" }) ➞ true
+
+// hasValidUnitOfMeasure({ "product": "Cereals", unitOfMeasure: "" }) ➞ false
+
+// hasValidUnitOfMeasure({ "product": "Beer", unitOfMeasure: false }) ➞ false
+
+// hasValidUnitOfMeasure({ "product": "Beef", unitOfMeasure: "Cow" }) ➞ false
+
+function hasValidUnitOfMeasure(obj) {
+    let measure = ["L", "PCE"]
+
+    if (measure.includes(obj.unitOfMeasure)) {
+        return true;
+    }
+    return false
+}
+console.log(hasValidUnitOfMeasure({ "product": "Beef", unitOfMeasure: "Cow" }));
+console.log(hasValidUnitOfMeasure({ "product": "Milk", unitOfMeasure: "L" }));
