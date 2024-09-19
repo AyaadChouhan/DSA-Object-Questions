8c4f3b69d7ffd55a40d5cd3c66e92dfc6d9d422b
Ayaad Chouhan <136963590+AyaadChouhan@users.noreply.github.com>
2024-09-20 01:28:37 +0530

    Update Question-41.js

--- MediumLevel-Questions/Question-41.js
+++ MediumLevel-Questions/Question-41.js
@@ -10,8 +10,50 @@
 
 // toObj([]) ➞ []
 
-
 function toObj(arr) {
-    
+  const alphabet = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+  ];
+  let obj = {};
+  let n = 97;
+  for (const val of alphabet) {
+    obj[val] = n;
+    n++;
+  }
+  let keys = Object.keys(obj);
+  let result = {};
+  for (let i = 0; i < arr.length; i++) {
+    for (const el of keys) {
+        if (arr[i] === el) {
+          result[arr[i]] = obj[el];
+        }     
+    }
+  }
+  return result;
 }
-console.log(toObj(["a", "b", "c"]))
+console.log(toObj(["a", "b", "c", "z"]));
