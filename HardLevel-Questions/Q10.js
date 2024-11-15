cc4159196bf01dac2c74a5d3d48b2815f9950e1a
Ayaad Chouhan <136963590+AyaadChouhan@users.noreply.github.com>
2024-11-16 01:02:06 +0530

    Update Q10.js

--- HardLevel-Questions/Q10.js
+++ HardLevel-Questions/Q10.js
@@ -1,30 +1,42 @@
 // Super Strict Grading
 // Given an object literal of student names and an array of their test scores over the semester, return a list of all the students who passed the course (in alphabetical order). However, there is one more thing to mention: the pass mark is 100% in everything!
 function whoPassed(obj) {
+  let newArr = [];
   for (const key in obj) {
-    console.log(typeof key)
-    // for (const el in key) {
-    //   let splitVal = el.split("/");
-    //   console.log(el);
-    // }
+    let booleanVal = false;
+    for (const el of obj[key]) {
+      let val = el.split("/");
+      if (val[0] / val[1] === 1) {
+        booleanVal = true;
+      } else {
+        booleanVal = false;
+        break;
+      }
+    }
+    if (booleanVal) {
+      newArr.push(key);
+    }
   }
+  return newArr;
 }
 
-whoPassed({
-  John: ["5/5", "50/50", "10/10", "10/10"],
-  Sarah: ["4/8", "50/57", "7/10", "10/18"],
-  Adam: ["8/10", "22/25", "3/5", "5/5"],
-  Barry: ["3/3", "20/20"],
-}); // ➞ ["Barry", "John"]
+console.log(
+  whoPassed({
+    John: ["5/5", "50/50", "10/10", "10/10"],
+    Sarah: ["4/8", "50/57", "7/10", "10/18"],
+    Adam: ["8/10", "22/25", "3/5", "5/5"],
+    Barry: ["3/3", "20/20"],
+  })
+); // ➞ ["Barry", "John"]
 
-whoPassed({
-  Zara: ["10/10"],
-  Kris: ["30/30"],
-  Charlie: ["100/100"],
-  Alex: ["1/1"],
-}); //➞ ["Alex", "Charlie", "Kris", "Zara"]
+// whoPassed({
+//   Zara: ["10/10"],
+//   Kris: ["30/30"],
+//   Charlie: ["100/100"],
+//   Alex: ["1/1"],
+// }); //➞ ["Alex", "Charlie", "Kris", "Zara"]
 
-whoPassed({
-  Zach: ["10/10", "2/4"],
-  Fred: ["7/9", "2/3"],
-});
+// whoPassed({
+//   Zach: ["10/10", "2/4"],
+//   Fred: ["7/9", "2/3"],
+// });
